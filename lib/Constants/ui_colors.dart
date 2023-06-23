4bec77f96e98362af64d940da265bfe871d4f1bf
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-23 08:04:35 -0700

    Add new colors

---
+++ lib/Constants/ui_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UIColors {
+  //Solid colors
+  static const black = Colors.black;
+  static const white = Colors.white;
+  static const dartBlue = Color(0xff0D47A1);
+  static const lightBlue = Color(0xff63B2F2);
+  static const lightRed = Color(0xffF26363);
+  //Text Colors
+  static const lightNormalText = Color.fromRGBO(0, 0, 0, 0.47);
+  static const warningText = Color(0xffE11E1E);
+  //Border colors
+  static const textFieldBorder = Color(0xff8F8989);
+  //Icon colors
+  static const warningIconColor = Color(0xffE11E1E);
+}
