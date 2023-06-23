baf32998eb2005dac8e0d65ad8f08aeaeb4153b3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-23 08:04:49 -0700

    Add new text styles

---
+++ lib/Constants/ui_text_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class UITextStyles {
+  static const normalTextStyle = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Inter',
+  );
+}
