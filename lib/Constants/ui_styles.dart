bed045f2d0ebcf39108c810b95519e22aca56921
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-06-23 08:05:09 -0700

    Add new button styles, text fields styles and border radius

---
+++ lib/Constants/ui_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:ossos_test_app/Constants/ui_colors.dart';
+import 'package:ossos_test_app/Constants/ui_text_style.dart';
+
+//Button Styles
+final normalButtonStyle = ButtonStyle(
+  elevation: const MaterialStatePropertyAll(0),
+  backgroundColor: const MaterialStatePropertyAll<Color>(UIColors.white),
+  shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+    RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(15),
+    ),
+  ),
+  minimumSize: const MaterialStatePropertyAll<Size>(
+    Size(double.infinity, 56),
+  ),
+);
+
+//Text Field Styles
+final normalInputStyle = InputDecoration(
+  border: UnderlineInputBorder(
+    borderSide: BorderSide(
+      width: 1,
+      color: UIColors.textFieldBorder,
+    ),
+  ),
+  hintStyle: UITextStyles.normalTextStyle,
+);
+
+//Border Radius
+const raduis15 = BorderRadius.all(Radius.circular(15));
